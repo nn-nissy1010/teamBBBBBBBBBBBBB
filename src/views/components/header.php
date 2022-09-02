06ea77b90d440c2e8fed233cd836510f6c5aec3e
Naoki Nishiyama <naoki1010nissy@gmail.com>
2022-09-02 10:20:05 +0900

    環境構築いったん

---
+++ src/views/components/header.php
@@ -0,0 +1,11 @@
+<?php
+// ヘッダー開始部分
+function a_header_start($content)
+{
+?>
+    <header class="header">
+        <div class="header-inner"><? $content?></div>
+    </header>
+<?php
+}
+?>
--- src/views/layouts/index.php
+++ src/views/layouts/index.php
@@ -1,14 +1,15 @@
-<?php require(realpath("config/dbconnect.php")); ?>
-
 <!DOCTYPE html>
-<html lang="en">
+<html lang="ja">
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="../sass/style.css">
 </head>
 <body>
-    <p>hello world</p>
+    
+
+<script src="../js/script.js"></script>
 </body>
 </html>

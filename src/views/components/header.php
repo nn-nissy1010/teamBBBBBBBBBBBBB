8df9e67fd2e5d6d6e638f00d5ade824af03ce099
田上黎 <tanouerei@tanouereinoMacBook-Air.local>
2022-09-02 14:14:34 +0900

    共通部分のヘッダー作成とヘッダーとフッターのパーツ化

--- src/views/components/header.php
+++ src/views/components/header.php
@@ -1,11 +1,18 @@
-<?php
-// ヘッダー開始部分
-function a_header_start($content)
-{
-?>
-    <header class="header">
-        <div class="header-inner"><? $content?></div>
-    </header>
-<?php
-}
-?>
+<!DOCTYPE html>
+<html lang="ja">
+
+<head prefix="og: http://ogp.me/ns# fb: http://ogp.me/ns/fb# website: http://ogp.me/ns/website#">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title><?= $title ?></title>
+    <link rel="stylesheet" href="../sass/style.css">
+    <link rel="stylesheet" href="../sass/createAccount.css">
+</head>
+
+<body>
+    <div class="body-wrapper">
+        <header>
+            <p style="text-align: center; font-size:36px; font-family:fantasy; margin-bottom:30px; color:#1E93FF">IDO<br>BTA</p>
+            <h1 class="label-title"><?= $title ?></h1>
+        </header>

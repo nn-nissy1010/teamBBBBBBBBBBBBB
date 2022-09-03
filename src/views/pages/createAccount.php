3e181aeb96f5ab83287709b4b4641a25b7f56777
田上黎 <tanouerei@tanouereinoMacBook-Air.local>
2022-09-03 22:18:16 +0900

    アカウント作成のバリデーション機能完成

--- src/views/pages/createAccount.php
+++ src/views/pages/createAccount.php
@@ -6,7 +6,7 @@
     require(realpath("../../config/dbconnect.php"));
 
 
-    if (isset($_POST)) {
+    if ($_POST) {
         $name = $_POST['name'];
         $tel = $_POST['tel'];
         $email = $_POST['email'];
@@ -17,14 +17,14 @@
     }
 ?>
 <?php include("../components/header.php"); ?>
-        <form class="form-wrapper" action="createAccount.php" method="post">
+        <form class="form-wrapper validation-form" action="createAccount.php" method="post" novalidate>
             <div class="form-sample">
                 <p class="form-label">氏名</p>
-                <input type="text" class="form-input" placeholder="例）鈴木一郎" name="name">
+                <input type="text" class="form-input required" placeholder="例）鈴木一郎" name="name">
             </div>
             <div class="form-sample">
                 <p class="form-label">電話番号</p>
-                <input type="text" class="form-input" placeholder="例）123-4567-8910" name="tel">
+                <input type="text" class="form-input required tel" placeholder="例）123-4567-8910" name="tel">
             </div>
             <div class="form-sample">
                 <p class="form-label">画像</p>
@@ -35,43 +35,19 @@
             </div>
             <div class="form-sample">
                 <p class="form-label">メールアドレス</p>
-                <input type="email" class="form-input" placeholder="例）sample@gmail.com" name="email">
+                <input type="email" class="form-input required email" placeholder="例）sample@gmail.com" name="email">
             </div>
             <div class="form-sample">
                 <p class="form-label">パスワード</p>
-                <input type="password" class="form-input" placeholder="例）password123" name="password" id="password" required>
+                <input type="password" class="form-input required" placeholder="例）password123" name="password1" id="password1" required>
             </div>
             <div class="form-sample">
                 <p class="form-label">パスワード再確認</p>
-                <input type="password" class="form-input" placeholder="例）password123" name="confirm" oninput="CheckPassword(this)" required>
+                <input type="password" class="form-input required equal-to" placeholder="例）password123" name="confirm" data-equal-to="password1" id="password2"required>
             </div>
             <input type="submit" class="form-Btn" value="会員登録">
         </form>
         <?php
 
-// 使用する変数を初期化
-    $name= '';
-    $tel= '';
-    $email= '';
-    $password= '';
-    $img= '';
 
-//エラー内容
-    $errors =[];
-    // 氏名
-    if (empty($_POST['name'])) {
-        $errors[] = '氏名は必須項目です。';
-    }
-    // 電話番号
-    if (empty($_POST['tel'])) {
-        $errors[] = '電話番号は必須項目です。';
-    }elseif(!preg_match( '/^0[0-9]{9,10}\z/', '文字列')){
-        $errors[] = "電話番号は0~9の値で入力してください。";
-    }
-    // Eメール
-    if (empty($_POST['email'])) {
-        $errors[] = 'Eメールは必須項目です。';
-    }elseif( !preg_match( '/^[0-9a-z_.\/?-]+@([0-9a-z-]+\.)+[0-9a-z-]+$/', $_POST['email']) ) {
-        $errors[] = "「メールアドレス」は正しい形式で入力してください。";
-    }
-     include("../components/footer.php"); ?>
+        include("../components/footer.php"); ?>

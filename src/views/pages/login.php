b741a099b7d459d544210de1c79b0c1a71da3bb8
kazuki663 <kazkaz.t@icloud.com>
2022-09-05 19:50:47 +0900

    バリデーションできた

--- src/views/pages/login.php
+++ src/views/pages/login.php
@@ -18,7 +18,34 @@ unset($_SESSION['img_path']);
 
 if (!empty($_POST)) {
   validateToken();
-  $login = $db->prepare('SELECT * FROM users WHERE email=? AND password=?');
+  $email = $_POST['email'];
+  $password = $_POST['password'];
+  if(isset($email) && isset($password)){
+  // eメールアドレスバリデーションチェック
+  // 空白チェック
+  if ($email === '') {
+    $err_msg['email'] = '入力必須です';
+  }
+  // 文字数チェック
+  elseif (strlen($email) > 255) {
+    $err_msg['email'] = '255文字で入力してください';
+  }
+  // パスワードバリデーションチェック
+  // 空白チェック
+  elseif ($password === '') {
+    $err_msg['password'] = '入力してください';
+  }
+  // 文字数チェック
+  elseif (strlen($password) > 255 || strlen($password) < 5) {
+    $err_msg['password'] = '６文字以上２５５文字以内で入力してください';
+  }
+  // 形式チェック
+  elseif (!preg_match("/^[a-zA-Z0-9]+$/", $password)) {
+    $err_msg['password'] = '半角英数字で入力してください';
+  }
+  
+  if (empty($err_msg)){
+    $login = $db->prepare('SELECT * FROM users WHERE email=? AND password=?');
   $login->execute(array(
     $_POST['email'],
     sha1($_POST['password'])
@@ -39,6 +66,12 @@ if (!empty($_POST)) {
     $error = 'fail';
     header("Location: http://" . $_SERVER['HTTP_HOST'] . "Location: /views/pages/login.php");
   }
+  }else{
+    foreach($err_msg as $err_msgs){
+      echo $err_msgs;
+    }
+  }
+}
 }
 
 ?>
